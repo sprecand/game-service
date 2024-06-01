d1e0aabeb3412c753d42ae2f876590a7e3a17098
sprecand <sprecand@students.zhaw.ch>
2024-06-01 17:46:03 +0200

    Added Spring Boot project

---
+++ src/main/java/ch/yass/game_service/GameServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.yass.game_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/yass/game_service/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.yass.game_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
