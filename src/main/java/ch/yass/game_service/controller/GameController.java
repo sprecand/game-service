25b46fa5b2664e91cef84cb1c273d4a01052c036
sprecand <sprecand@students.zhaw.ch>
2024-06-01 18:49:24 +0200

    Added Controller

---
+++ src/main/java/ch/yass/game_service/controller/GameController.java
@@ -0,0 +1,17 @@
+package ch.yass.game_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
+@RestController
+public class GameController {
+	@Operation(summary = "Greet the user")
+	@GetMapping("/api/greeting")
+	public String greeting(
+			@Parameter(description = "Name of the person to greet") @RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello " + name;
+	}
+}
